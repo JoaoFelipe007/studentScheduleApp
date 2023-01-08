005b580806165cd1c82667cec2cda9a76cda9b8e
JoaoFelipe007 <joaofelipedasilva687@gmail.com>
2023-01-08 16:00:41 -0300

    feat: menu de contexto para a remoção

--- app/src/main/java/alura/com/br/ui/activity/ListStudentActivity.java
+++ app/src/main/java/alura/com/br/ui/activity/ListStudentActivity.java
@@ -5,12 +5,15 @@ import static alura.com.br.R.layout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -42,7 +45,33 @@ public class ListStudentActivity extends AppCompatActivity {
          só precisam ser executadas uma unica vez e não toda
          s as vezes que a activity entra em on resume*/
         configuraFabNovoAluno();
+    }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {// Cria um menu de contexto
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.activity_list_student_menu, menu);/*Vincula a activity de menu, usando uma tecnica de
+        inflar (Vai pegar o arquivo estatico e vai transformar em um onjeto de menu)*/
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();//pega o id do menu
+        if (itemId == R.id.activity_list_student_menu_remove) {
+            AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();/* .getmenuInfo é usado para pegar mais
+       informações... porém do jeito que está e de um modo especifico para AdapterViews*/
+            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+            dao.remove(alunoEscolhido);
+            adapter.remove(alunoEscolhido);
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.clear();//Enquanto tiver neeste estado ele ira limapr os dados da listView
+        adapter.addAll(dao.todos());//E logo após ele ira adcionar tudo novamente na listView
     }
 
     private void configuraFabNovoAluno() {
@@ -55,37 +84,19 @@ public class ListStudentActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        adapter.clear();//Enquanto tiver neeste estado ele ira limapr os dados da listView
-        adapter.addAll(dao.todos());//E logo após ele ira adcionar tudo novamente na listView
-    }
 
     private void listaAlunos() {
         ListView listaAluno = findViewById(R.id.acitivity_list_students_listview);
         configuraAdapter(listaAluno);
         configuraClickPorItem(listaAluno);
-        configuraClickLongPorItem(listaAluno);
-    }
-
-    private void configuraClickLongPorItem(ListView listaAluno) {
-        listaAluno.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Aluno alunoEscolhido = (Aluno)parent.getItemAtPosition(position);
-                dao.remove(alunoEscolhido);
-                adapter.remove(alunoEscolhido);
-                return false;
-            }
-        });
+        registerForContextMenu(listaAluno);
     }
 
     private void configuraAdapter(ListView listaAluno) {
         adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1
-                );//deixei o adpter como uma variavel global para que possa ser usada em outra situaçõe
+        );//deixei o adpter como uma variavel global para que possa ser usada em outra situaçõe
         listaAluno.setAdapter(adapter);
     }
 
@@ -95,8 +106,8 @@ public class ListStudentActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Aluno alunoEscolhido = (Aluno) parent.getItemAtPosition(position); //com base no item selecionado ele consegue retornar um objeto
-                Intent vaiParaFormularioActivity =new Intent(ListStudentActivity.this,FormStudentActivity.class);
-                vaiParaFormularioActivity.putExtra("aluno",alunoEscolhido);
+                Intent vaiParaFormularioActivity = new Intent(ListStudentActivity.this, FormStudentActivity.class);
+                vaiParaFormularioActivity.putExtra("aluno", alunoEscolhido);
                 /* através do putExtra vc manda o objeto e suas informações para a outra activity
                   e nele vc também manda uma chave, para que ser usada na outra activity
                 *  só pode ser usado por uma varivel do tipo intent
